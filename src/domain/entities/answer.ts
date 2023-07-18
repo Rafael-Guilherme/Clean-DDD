f56137a77a3cccd0a369746f7d6f0eed8b0095f1
Rafael <rafael_guilherme@hotmail.com>
2023-07-18 17:01:51 -0300

    feat: create answer repository

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,11 +1,21 @@
 import { randomUUID } from "node:crypto";
 
+interface AnswerProps {
+    content: string;
+    authorId: string; 
+    questionId: string;
+}
+
 export class Answer {
     public content: string;
     public id: string;
+    public authorID: string;
+    public questionID: string;
 
-    constructor(content: string, id?: string) {
-        this.content = content;
+    constructor(props: AnswerProps, id?: string) {
+        this.content = props.content;
+        this.authorID = props.authorId;
+        this.questionID = props.questionId;
         this.id = id ?? randomUUID();
     }
 }
